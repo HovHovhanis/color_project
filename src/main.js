75bf9446008bac257802f0e910cf3fc7009a9d49
Hovhannes <hhovhannisyan348@gmail.com>
2025-07-14 18:31:34 +0400

    Fix assets and SCSS structure

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import './assets/scss/main.scss';
+import './scss/main.scss';
 import { loadProducts } from './components/products/productCard.js';
 import { initHeroSlider } from './components/hero/hero.js';
 import { initHeader } from './components/header/header.js'
